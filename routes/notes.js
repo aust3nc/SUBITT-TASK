92011aff6074a6585312d6f34d83551f0d5ab0b2
Austen Cheethirala <87336133+aust3nc@users.noreply.github.com>
2022-10-27 18:21:44 -0600

    Update notes.js
    Added status codes

--- routes/notes.js
+++ routes/notes.js
@@ -3,44 +3,91 @@ const Note = require('./../models/note')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-  res.render('notes/new', { note: new Note() })
+  try {
+    res.render('notes/new', { note: new Note() })
+    res.status(200)
+  } catch (e) {
+    res.redirect('/')
+    res.status(500).send('Status: Internal Server Error');
+  }
 })
 
 router.get('/edit/:id', async (req, res) => {
-  const note = await Note.findOneAndDelete(req.params.id)
-  res.render('notes/edit', { note: note })
+  try {
+    const note = await Note.findById(req.params.id)
+    res.status(200)
+    res.render('notes/edit', { note: note })
+  } catch (e) {
+    res.redirect('/')
+    res.status(404).send('Status: Not Found');
+  }
 })
 
 router.get('/replace/:id', async (req, res) => {
-  const note = await Note.findByIdAndDelete(req.params.id)
-  res.render('notes/replace', { note: note })
+  try {
+    const note = await Note.findById(req.params.id)
+    res.status(200)
+    res.render('notes/replace', { note: note })
+  } catch (e) {
+    res.redirect('/')
+    res.status(404).send('Status: Not Found');
+  }
 })
 
 router.get('/:slug', async (req, res) => {
-  const note = await Note.findOne({ slug: req.params.slug })
-  if (note == null) res.redirect('/')
-  res.render('notes/show', { note: note })
+  try {
+    const note = await Note.findOne({ slug: req.params.slug })
+    res.status(200)
+    res.render('notes/show', { note: note })
+  } catch (e) {
+    res.redirect('/')
+    res.status(404).send('Status: Not Found');
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  req.note = new Note()
-  next()
+  try {
+    req.note = new Note()
+    next()
+  } catch (e) {
+    res.redirect('/')
+    res.status(500).send('Status: Internal Server Error');
+  }
 }, saveNoteAndRedirect('new'))
 
 router.put('/:id', async (req, res, next) => {
-  req.note = await Note.findById(req.params.id)
-  next()
+  try {
+    req.note = await Note.findById(req.params.id)
+    res.status(201)
+    next()
+  }
+  catch (e) {
+    res.redirect('/')
+    res.status(500).send('Status: Internal Server Error');
+  }
 }, saveNoteAndRedirect('edit'))
 
 router.put('/:id', async (req, res, next) => {
-  req.note = await Note.findOneAndReplace(req.params.id)
-  next()
+  try {
+    req.note = await Note.findById(req.params.id)
+    res.status(201)
+    next()
+  } catch (e) {
+    res.redirect('/')
+    res.status(500).send('Status: Internal Server Error');
+  }
 }, saveNoteAndRedirect('replace'))
 
 router.delete('/:id', async (req, res) => {
-  await Note.findByIdAndDelete(req.params.id)
-  res.redirect('/')
-})
+  try { 
+    await Note.findByIdAndRemove(req.params.id)
+    res.status(201)
+    res.redirect('/')
+  } catch (e) {
+    res.redirect('/')
+    res.status(500).send('Status: Internal Server Error');
+  }
+}) 
 
 function saveNoteAndRedirect(path) {
   return async (req, res) => {
@@ -51,9 +98,9 @@ function saveNoteAndRedirect(path) {
     })
     try {
       note = await note.save()
+      res.status(201)
       res.redirect(`/notes/${note.slug}`)
     } catch (e) {
-      // console.log(e)
       res.render(`notes/${path}`, { note: note })
     }
   }
